b7d19ef4825ccd5c91373bdf5d77844884529062
gaaran <42284498+MagitekZed@users.noreply.github.com>
2023-12-11 18:43:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-def hello_world():
-    return 'Hello, World!'
+@socketio.on('change_color')
+def handle_change_color():
+    global current_color
+    current_color = "red" if current_color == "blue" else "blue"
+    emit('color_change', {'color': current_color}, broadcast=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
