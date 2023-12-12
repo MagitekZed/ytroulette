36518421ccecffc61bb7b0529f281b08cef62d0d
gaaran <42284498+MagitekZed@users.noreply.github.com>
2023-12-11 21:51:05 -0800

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -1,1 +1,20 @@
+import random
+import string
 
+games = {}
+
+def generate_game_code(length=4):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+def create_game(player_name):
+    code = generate_game_code()
+    games[code] = {'players': {player_name}, 'active': True}
+    return code
+
+def join_game(code, player_name):
+    if code in games and player_name not in games[code]['players']:
+        games[code]['players'].add(player_name)
+        return True
+    return False
+
+# Additional game logic functions here
