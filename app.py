5ccf57d831ddcc28bcdeed597a68da2cc3d9988a
gaaran <42284498+MagitekZed@users.noreply.github.com>
2023-12-11 17:33:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
 def hello_world():
     return 'Hello, World!'
 
