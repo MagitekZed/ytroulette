b93d9b99575e54aaed2f2cec8ff51830e01b635d
gaaran <42284498+MagitekZed@users.noreply.github.com>
2023-12-13 14:52:46 -0800

    Create game_setup.js

---
+++ static/game_setup.js
@@ -0,0 +1,19 @@
+function showCreateGameForm() {
+    document.getElementById('rulesBox').style.display = 'none';
+    document.getElementById('createGameForm').style.display = 'block';
+}
+
+function showJoinGameForm() {
+    document.getElementById('rulesBox').style.display = 'none';
+    document.getElementById('joinGameForm').style.display = 'block';
+}
+
+function createGame() {
+    // Implement AJAX call to server to create game
+}
+
+function joinGame() {
+    // Implement AJAX call to server to join game
+}
+
+// Add event listeners for form submission if necessary
