953ea6165fcb8a59dcd6b93477119d4c58c8cfe8
MagitekZed <42284498+MagitekZed@users.noreply.github.com>
2023-12-13 22:25:43 +0000

    getting it working on replit

--- app.py
+++ app.py
@@ -49,5 +49,6 @@ def game(game_code):
 # Add more route definitions here
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.1', port=8080)
+  app.run(host='0.0.0.0', port=8080)  # Replit uses port 8080
+
 
